035b0fbde3173ab5ccca354e88417ddf301810c2
itzsebas121 <sebastipan121@gmail.com>
2025-06-11 18:15:54 -0500

    commit 6:15

--- app/src/main/java/com/example/prueba02_01_rene_tipan/MainActivity.java
+++ app/src/main/java/com/example/prueba02_01_rene_tipan/MainActivity.java
@@ -1,24 +1,30 @@
 package com.example.prueba02_01_rene_tipan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText rstn_input;
+    Button rstn_btnEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        rstn_input = findViewById(R.id.rstn_input);
+        rstn_btnEnviar = findViewById(R.id.rstn_btnEnviar);
+
+        rstn_btnEnviar.setOnClickListener(v -> {
+            String mensaje = rstn_input.getText().toString();
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            intent.putExtra("rstn_dato", mensaje);
+            startActivity(intent);
         });
     }
 }
